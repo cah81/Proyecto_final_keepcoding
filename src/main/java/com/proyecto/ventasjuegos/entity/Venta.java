57cb34c423abb1df6454719b8e783e01e74d8e71
carlos angulo <carlosenriqueanguloherrera@gmail.com>
2022-07-20 20:57:54 +0200

    Se agregan los entitys , el archivo de conexion

---
+++ src/main/java/com/proyecto/ventasjuegos/entity/Venta.java
@@ -0,0 +1,82 @@
+package com.proyecto.ventasjuegos.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "venta")
+public class Venta implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@Column(nullable = false, unique = true)
+	private String nroFactura;
+	@Column(nullable = false)
+	private Date fechaVenta;
+	@Column(nullable = false)
+	private double monto;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id_Cliente")
+	@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
+	private Cliente cliente;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNroFactura() {
+		return nroFactura;
+	}
+
+	public void setNroFactura(String nroFactura) {
+		this.nroFactura = nroFactura;
+	}
+
+	public Date getFechaVenta() {
+		return fechaVenta;
+	}
+
+	public void setFechaVenta(Date fechaVenta) {
+		this.fechaVenta = fechaVenta;
+	}
+
+	public double getMonto() {
+		return monto;
+	}
+
+	public void setMonto(double monto) {
+		this.monto = monto;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
