0ead6cb46f3f2e0b27c82b11cd9f47fdd5573095
carlos angulo <carlosenriqueanguloherrera@gmail.com>
2022-07-23 17:56:22 +0200

    Actualizacion con controlador de proveedor

--- src/main/java/com/proyecto/ventasjuegos/apirest/controller/ProductoRestController.java
+++ src/main/java/com/proyecto/ventasjuegos/apirest/controller/ProductoRestController.java
@@ -16,6 +16,7 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,7 +29,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import com.proyecto.ventasjuegos.apirest.entity.Producto;
 
-
+@CrossOrigin(origins = "http://localhost:4200/")
 @RestController
 @RequestMapping("/api")
 public class ProductoRestController {
