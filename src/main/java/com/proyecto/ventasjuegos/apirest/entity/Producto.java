0dcdb48082cc5600ed36fc31b7bc25b5f2924cec
rocio <rociordev@gmail.com>
2022-07-23 23:02:45 +0200

    Arreglado producto entity

--- src/main/java/com/proyecto/ventasjuegos/apirest/entity/Producto.java
+++ src/main/java/com/proyecto/ventasjuegos/apirest/entity/Producto.java
@@ -1,7 +1,6 @@
 package com.proyecto.ventasjuegos.apirest.entity;
 
-
-
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -11,7 +10,6 @@ import javax.persistence.*;
 @Entity
 @Table(name = "producto")
 @Data
-
 public class Producto implements Serializable {
 
 	@Id
@@ -22,25 +20,26 @@ public class Producto implements Serializable {
 	@Column(nullable = false, unique = true)
 	private String codigo_producto;
 	@Column(nullable = false)
-	private String tipo;
-	@Column(nullable = false)
 	private String descripcion;
-	//@Column(nullable = false)
+	@Column(nullable = false)
 	private String imagen;
 	@Column(nullable = false)
-	private int stock;
-
+	private int cantidad;
 	@Column(nullable = false)
-	private double precio;
+	private String tipo;
+	@Column(nullable = false)
+	private int precio;
+	@Column(nullable = false)
+	private int fecha_registro;
 
-	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id_Proveedor")
+	@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+	private Proveedor proveedor;
 
-	
-
-	
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
 
